97103904652912ab6cded821958bcad76436c05b
Vrushabh Patil <vrushabhpatil4801@gmail.com>
2024-10-03 22:45:34 +0530

    defined schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,6 +31,14 @@ app.get('/api/user/results', (c) => {
   return c.text('Result route')
 })
 
+app.get('/api/user/quiz/:id', (c) => {
+  return c.text('Fetch quiz details route');
+});
+
+app.get('/api/user/quiz/:id/leaderboard', (c) => {
+  return c.text('Fetch leaderboard route');
+});
+
 //admin routes
 app.post('/api/admin/signup', (c) => {
   return c.text('Admin Signup route');
@@ -51,5 +59,22 @@ app.delete('/api/admin/delete', (c) => {
 app.get('/api/admin/dashboard', (c) => {
   return c.text('Admin Dashboard route');
 })
+
+app.put('/api/admin/quiz/:id', (c) => {
+  return c.text('Update quiz route');
+});
+
+app.get('/api/admin/quizzes', (c) => {
+  return c.text('Fetch all quizzes route');
+});
+
+app.get('/api/admin/quiz/:id/results', (c) => {
+  return c.text('Fetch quiz results route');
+});
+
+app.put('/api/admin/quiz/:id/end', (c) => {
+  return c.text('End quiz route');
+});
+
 
 export default app
